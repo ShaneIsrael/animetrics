806c792af46a4da657a72fc3e9d986233af7b9c7
Shane Israel <sisrael@sofi.org>
2019-11-30 21:42:33 -0700

    add start script and prod config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -20,6 +20,26 @@ module.exports = {
       access_key_secret: '54ORrWz/8A/FKA3z9lTVYAXUaJ8xb8q4tQz73TvS070',
     },
   },
+  prod: {
+    reddit: {
+      CLIENT_ID: 'o1P2JRGCX5FQlA',
+      CLIENT_SECRET: 'zGvwomVvHhEzqgSrXP8dhUuFzSY',
+      REFRESH_TOKEN: '10419841-pv1qeYS2Y8t0LhHSYQzyaOjLySk',
+    },
+    tvdb: {
+      API_KEY: '364A3D46-3293-42EF-891B-F671ACFD9A75',
+    },
+    assets: {
+      imagesRootPath: '/temp',
+      detectFacePath: '/var/www/animetrics/server/src/tools/detectFace.py',
+      detectFaceConfPath: '/var/www/animetrics/server/src/config/libcascade_animeface.xml',
+    },
+    aws: {
+      endpoint: 'sfo2.digitaloceanspaces.com',
+      access_key_id: 'SHW64UJLEICUJPDMHTSP',
+      access_key_secret: '54ORrWz/8A/FKA3z9lTVYAXUaJ8xb8q4tQz73TvS070',
+    },
+  },
   mac: {
     reddit: {
       CLIENT_ID: 'o1P2JRGCX5FQlA',
