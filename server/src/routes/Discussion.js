3a49d1988b3a8ca842b8e427c1406916fe492120
shaneisrael <shanemisrael@gmail.com>
2019-12-15 21:29:43 -0700

    added recent discussion route which gives last 15 discussions

--- server/src/routes/Discussion.js
+++ server/src/routes/Discussion.js
@@ -1,7 +1,9 @@
 const {
   getTodaysDiscussions,
+  getRecentDiscussions,
 } = require('../controllers')
 
 module.exports = (app) => {
   app.get('/api/v1/discussion/today', getTodaysDiscussions)
+  app.get('/api/v1/discussion/recent', getRecentDiscussions)
 }
