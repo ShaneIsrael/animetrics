41de38924e3119cd9a4eeb2509f88170d7a85020
Shane Israel <sisrael@sofi.org>
2019-12-17 13:56:08 -0700

    added better logging

--- server/src/logger/index.js
+++ server/src/logger/index.js
@@ -1,5 +1,5 @@
 const appRoot = require('app-root-path')
-const winston = require('winston')
+const { transports, createLogger, format} = require('winston')
 
 // define the custom settings for each transport (file, console)
 const options = {
@@ -11,20 +11,37 @@ const options = {
     maxsize: 5242880, // 5MB
     maxFiles: 5,
     colorize: false,
+    timestamp: true,
+  },
+  errorFile: {
+    level: 'error',
+    filename: `${appRoot}/logs/error.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+    timestamp: true,
   },
   console: {
     level: 'debug',
     handleExceptions: true,
     json: false,
     colorize: true,
+    timestamp: true,
   },
 }
 
 // instantiate a new Winston Logger with the settings defined above
-const logger = winston.createLogger({
+const logger = createLogger({
+  format: format.combine(
+    format.timestamp(),
+    format.json(),
+  ),
   transports: [
-    new winston.transports.File(options.file),
-    new winston.transports.Console(options.console),
+    new transports.File(options.file),
+    new transports.File(options.errorFile),
+    new transports.Console(options.console),
   ],
   exitOnError: false, // do not exit on handled exceptions
 })
