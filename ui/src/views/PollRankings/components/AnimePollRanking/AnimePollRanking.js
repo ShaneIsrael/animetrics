42327ede5ff2e1def9f17ea138c4e3717073d23a
Shane Israel <sisrael@sofi.org>
2019-12-19 14:10:24 -0700

    desktop poll rankings cards

--- ui/src/views/PollRankings/components/AnimePollRanking/AnimePollRanking.js
+++ ui/src/views/PollRankings/components/AnimePollRanking/AnimePollRanking.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/styles'
-import { Grid, Paper, Typography, Hidden } from '@material-ui/core'
+import { makeStyles, withStyles } from '@material-ui/styles'
+import { Grid, Paper, Typography, Hidden, Tooltip } from '@material-ui/core'
 import clsx from 'clsx'
-import { deepOrange, deepPurple } from '@material-ui/core/colors'
+import { deepOrange, deepPurple, lightBlue } from '@material-ui/core/colors'
 import Chip from '@material-ui/core/Chip'
 import Avatar from '@material-ui/core/Avatar'
 import RemoveIcon from '@material-ui/icons/Remove'
@@ -14,7 +14,17 @@ import ThumbsUpDownIcon from '@material-ui/icons/ThumbsUpDown'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
+import Zoom from '@material-ui/core/Zoom'
 import LazyLoad from 'react-lazy-load'
+
+const LightTooltip = withStyles(theme => ({
+  tooltip: {
+    backgroundColor: theme.palette.common.white,
+    color: 'rgba(0, 0, 0, 0.87)',
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+  },
+}))(Tooltip)
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -151,10 +161,62 @@ const useStyles = makeStyles(theme => ({
     borderRadius: 0,
     margin: theme.spacing(0.5)
   },
+  rankColor: {
+    color: lightBlue['A400'],
+    borderColor: lightBlue['A400'],
+    backgroundColor: 'rgba(0,176,255,0.2)'
+  },
+}))
+
+const desktopStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    minWidth: 415,
+  },
+  upIcon: {
+    transform: 'rotate(-90deg)',
+    WebkitTextStroke: '1px',
+    WebkitTextStrokeColor: 'black',
+  },
+  downIcon: {
+    transform: 'rotate(90deg)',
+  },
+  orangeColor: {
+    color: deepOrange[500],
+    borderColor: deepOrange[500]
+  },
+  purpleColor: {
+    color: deepPurple[300],
+    borderColor: deepPurple[300]
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+  },
+  card: {
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    borderRadius: 0,
+    width: 450,
+    height: 155,
+  },
+  cardHeader: {
+    padding: 8,
+  },
+  cardContent: {
+    padding: 8,
+  },
+  squareChip: {
+    borderRadius: 0,
+    margin: theme.spacing(0.5)
+  },
+  positionChipFont: {
+    fontSize: 24
+  },
 }))
 //eslint-disable-next-line react/jsx-max-props-per-line
 const AnimePollRanking = (props) => {
   const classes = useStyles()
+  const desktop = desktopStyles()
 
   let { current } = props
   
@@ -177,13 +239,15 @@ const AnimePollRanking = (props) => {
             }
             title={current.show.title}
             subheader={`Episode ${current.discussion.episode}`}
+            subheaderTypographyProps={{color: 'primary'}}
           />
           <CardContent className={classes.cardContent}>
             <React.Fragment>
               <Chip
-                className={clsx({[classes.squareChip]: true})}
+                className={clsx({[classes.squareChip]: true, [classes.rankColor]: true})}
                 avatar={<PollIcon />}
                 label={current.score}
+                variant="outlined"
               />
               {(posChangeUp || posChangeDown) &&
                 <Chip
@@ -208,7 +272,57 @@ const AnimePollRanking = (props) => {
         implementation="js"
         smDown
       >
-        <div className={clsx(classes.root, 'grow')}>
+        <Card className={desktop.card}>
+          <CardHeader
+            className={desktop.cardHeader}
+            avatar={
+              <LazyLoad debounce={true} offsetVertical={400} width={80}><Avatar className={desktop.avatar} src={avatar} /></LazyLoad>
+            }
+            title={current.show.title}
+            titleTypographyProps={{variant: 'h6', component: 'h6'}}
+            subheader={`Episode ${current.discussion.episode}`}
+            subheaderTypographyProps={{color: 'primary'}}
+          />
+          <CardContent className={desktop.cardContent}>
+            <React.Fragment>
+              <LightTooltip enterDelay={500} TransitionComponent={Zoom} title="Current Rank" placement="top">
+                <Chip
+                  className={clsx({[desktop.squareChip]: true, [desktop.positionChipFont]: true, [classes.rankColor]: true})}
+                  label={(current.position + 1)}
+                  variant="outlined"
+                />
+              </LightTooltip>
+              <LightTooltip enterDelay={500} TransitionComponent={Zoom} title="Episode Poll Result" placement="top">
+                <Chip
+                  className={clsx({[desktop.squareChip]: true, [classes.rankColor]: true})}
+                  avatar={<PollIcon />}
+                  label={current.score}
+                  variant="outlined"
+                />
+              </LightTooltip>
+              {(posChangeUp || posChangeDown) &&
+                <LightTooltip enterDelay={500} TransitionComponent={Zoom} title="Episode Poll Result Change" placement="top">
+                  <Chip
+                    className={clsx({[desktop.squareChip]: true, [desktop.orangeColor]: posChangeUp, 
+                      [desktop.purpleColor]: posChangeDown})}
+                    avatar={<ForwardIcon className={clsx({[desktop.upIcon]: posChangeUp, [desktop.downIcon]: posChangeDown})}/>}
+                    label={Math.abs(current.position - current.previous.position)}
+                    variant="outlined"
+                  />
+                </LightTooltip>
+              }
+              <LightTooltip enterDelay={500} TransitionComponent={Zoom} title="Total Votes in Poll" placement="top">
+                <Chip
+                  className={clsx({[desktop.squareChip]: true})}
+                  avatar={<ThumbsUpDownIcon/>}
+                  label={current.votes}
+                  variant="outlined"
+                />
+              </LightTooltip>
+            </React.Fragment>
+          </CardContent>
+        </Card>
+        {/* <div className={clsx(classes.root, 'grow')}>
           <Paper
             className={classes.paper}
             square
@@ -293,7 +407,7 @@ const AnimePollRanking = (props) => {
               </Grid>
             </Grid>
           </Paper>
-        </div>
+        </div> */}
       </Hidden>
     </Grid>
   )
