e30880e718d7fb6fe8c02e9e60c0001bec7dd4ce
shaneisrael <shanemisrael@gmail.com>
2019-12-24 22:50:06 -0700

    uncomment

--- server/src/tools/test.js
+++ server/src/tools/test.js
@@ -7,24 +7,24 @@ const {
 } = require('../models')
 const fetchAssets = require('../fetch/fetchAssets')
 
-// async function updatePosters() {
-//   await authTvDb()
-//   const assets = await Asset.findAll({
-//     where: {
-//       poster_art: null,
-//     },
-//     include: [Show],
-//   })
-//   for (const asset of assets) {
-//     const art = await getSeriesPoster(asset.Show.tvdb_id)
-//     if (art === null) {
-//       console.log(asset.Show.title, asset.Show.tvdb_id)
-//     } else {
-//       asset.poster_art = art
-//       asset.save()
-//     }
-//   }
-//   await fetchAssets.fetch()
-// }
-// updatePosters()
+async function updatePosters() {
+  await authTvDb()
+  const assets = await Asset.findAll({
+    where: {
+      poster_art: null,
+    },
+    include: [Show],
+  })
+  for (const asset of assets) {
+    const art = await getSeriesPoster(asset.Show.tvdb_id)
+    if (art === null) {
+      console.log(asset.Show.title, asset.Show.tvdb_id)
+    } else {
+      asset.poster_art = art
+      asset.save()
+    }
+  }
+  await fetchAssets.fetch()
+}
+updatePosters()
 
