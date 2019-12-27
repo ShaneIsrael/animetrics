71ec9593583d0a359506aef7ffc23783f36bb651
shaneisrael <shanemisrael@gmail.com>
2019-12-27 09:14:39 -0700

    added feedback dialog, no wiring

---
+++ ui/src/components/Dialog/FeedbackDialog.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItem from '@material-ui/core/ListItem'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import CloseIcon from '@material-ui/icons/Close'
+import Slide from '@material-ui/core/Slide'
+import { DialogContent, DialogContentText, TextField } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+  textField: {
+    height: '100%'
+  }
+}))
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+})
+
+export default function FeedbackDialog(props) {
+  const classes = useStyles()
+
+  const [ text, setText ] = React.useState('')
+  const { handleClose, open } = props
+
+  const maxLength = 1000
+
+  const handleChange = (event) => {
+    const length = event.target.value.length
+    if (length <= maxLength) {
+      setText(event.target.value)
+    } else {
+      setText(event.target.value.slice(0, maxLength))
+    }
+  }
+  return (
+    <div>
+      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Feedback
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              Submit
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <DialogContent>
+          <DialogContentText>
+            Your feedback will help me continue to improve Animetrics. If there is something you like or do not like, please let me know. If there is a feature that you would like to see I would love to hear about it. Thanks!
+          </DialogContentText>
+          <TextField
+            id="feedbackField"
+            label={`${text.length}/${maxLength}`}
+            variant="outlined"
+            multiline
+            rows={10}
+            fullWidth={true}
+            value={text}
+            onChange={handleChange}
+            className={classes.textField}
+          >
+
+            
+          </TextField>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
