fa299377f1e9cb60eb220216c30129b98d88d3e5
shaneisrael <shanemisrael@gmail.com>
2019-12-30 19:04:57 -0700

    lazy load image component that has placeholder progress bar

---
+++ ui/src/components/LazyLoadImage/LazyLoadImage.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import LazyLoad from 'react-lazy-load'
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: ({ loadWidth }) => loadWidth,
+    height: ({ loadHeight }) => loadHeight,
+
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}))
+
+export default function LazyLoadImage(props) {
+  const { src, altComponent, width, alt, verticalOffset, className } = props
+  let {  loadWidth, loadHeight } = props
+  loadWidth = loadWidth ? loadWidth : '100%'
+  loadHeight = loadHeight ? loadHeight : '100%'
+  const classes = useStyles({ loadWidth, loadHeight })
+  const [loaded, setLoaded] = React.useState(false)
+
+  let defaultComponent = (
+    <div className={classes.root}>
+      <CircularProgress />
+    </div>
+  )
+  if (altComponent) {
+    defaultComponent = altComponent
+  }
+  return (
+    <LazyLoad
+      debounce={false}
+      offsetVertical={verticalOffset ? verticalOffset : 400}
+      width={width}
+    >
+      {loaded ?
+        <img style={{display: `${loaded ? 'block' : 'none'}`}} className={loaded ? className : ''} alt={alt} width={width} src={src} onLoad={() => setLoaded(true)}/>
+        :
+        defaultComponent
+      }
+    </LazyLoad>
+  )
+}
