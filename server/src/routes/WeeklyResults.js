58ad52fa24dab872d84776d551e8fd16af54d83a
shaneisrael <shanemisrael@gmail.com>
2020-01-04 00:33:52 -0700

    fix issue where I overrode a route

--- server/src/routes/WeeklyResults.js
+++ server/src/routes/WeeklyResults.js
@@ -10,6 +10,6 @@ module.exports = (app) => {
   app.get('/api/v1/weeks', getWeeks)
   app.get('/api/v1/weeks/season', getWeeksBySeason)
   app.get('/api/v1/results/:order/:week', getResultsByOrderAndWeek)
-  app.get('/api/v1/results', getResultsByWeek)
-  app.get('/api/v1/results/poll/week', getRedditPollResultsByWeek)
+  app.get('/api/v1/karma-ranks/week', getResultsByWeek)
+  app.get('/api/v1/poll-ranks/week', getRedditPollResultsByWeek)
 }
