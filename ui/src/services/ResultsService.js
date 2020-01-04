f1b96640698338e9e9d74f2e6e94a53194b8901b
shaneisrael <shanemisrael@gmail.com>
2020-01-04 00:33:40 -0700

    fix issue where I overrode a route

--- ui/src/services/ResultsService.js
+++ ui/src/services/ResultsService.js
@@ -2,12 +2,12 @@ import Api from './Api'
 
 export default {
   getResultsByWeek(id) {
-    return Api().get('/api/v1/results', {
+    return Api().get('/api/v1/karma-ranks/week', {
       params: {id}
     })
   },
   getRedditPollResultsByWeek(id) {
-    return Api().get('/api/v1/results/poll/week', {
+    return Api().get('/api/v1/poll-ranks/week', {
       params: {id}
     })
   }
