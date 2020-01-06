99c50489d569c0ce82988373db615f9dfe98876d
Shane Israel <sisrael@sofi.org>
2020-01-06 16:05:38 -0700

    added test

--- server/src/tools/test.js
+++ server/src/tools/test.js
@@ -1,10 +1,50 @@
+const service = {}
+const moment = require('moment')
+const { findAnime, rawTvDbSearch, authTvDb } = require('../services')
+const fetchDiscussions = require('../fetch/fetchDiscussions')
+const fetchAssets = require('../fetch/fetchAssets')
+const cpoll = require('../tools/calculatePoll')
+const logger = require('../logger')
+const { Show, Asset, MALSnapshot, RedditPollResult, Week, Season, EpisodeResultLink, EpisodeDiscussion, EpisodeDiscussionResult, RedditUserScore, Op} = require('../models')
+
 const Anilist = require('anilist-node')
 const aniClient = new Anilist()
 
 //create animetrics anilist app 
 
-async function test() {
-  const resp = await aniClient.media.anime(101350)
-  console.log(resp.coverImage.large)
+function getAnilistUrl(text) {
+  const url = text.match(/https?:\/\/(www\.)?(\w*anilist\w*)\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]anime\/[0-9]*)/gm)
+  return url ? url[0] : null
 }
-test()
+
+function parseAnilistId(post) {
+  const anilistUrl = getAnilistUrl(post.selftext)
+  if (anilistUrl) {
+    const anilistId = anilistUrl.match(/([0-9]\d+)/g)
+    return anilistId ? anilistId[0] : null
+  }
+  return null
+}
+
+async function fixMyHeroAcademia() {
+  try {
+    const shows = await Show.findAll({
+      where: {
+        anilist_id: null
+      },
+      include: [EpisodeDiscussion],
+    })
+    for (const show of shows) {
+      const post = await fetchDiscussions.getSubmission(show.post_id)
+      if (post) {
+        const id = parseAnilistId(post)
+        // show.anilist_id = id
+        // show.save()
+        console.log(id)
+      }
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+fixMyHeroAcademia()
