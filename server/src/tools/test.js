82dba3a28ed3cbd6f3760e797127ee82857da1d0
Shane Israel <sisrael@sofi.org>
2020-01-07 13:21:42 -0700

    use synopsis from malDetails

--- server/src/tools/test.js
+++ server/src/tools/test.js
@@ -1,16 +1,18 @@
 const service = {}
-const moment = require('moment')
-const { findAnime, rawTvDbSearch, authTvDb } = require('../services')
-const fetchDiscussions = require('../fetch/fetchDiscussions')
-const fetchAssets = require('../fetch/fetchAssets')
-const cpoll = require('../tools/calculatePoll')
-const logger = require('../logger')
-const { Show, Asset, MALSnapshot, RedditPollResult, Week, Season, EpisodeResultLink, EpisodeDiscussion, EpisodeDiscussionResult, RedditUserScore, Op} = require('../models')
-
+// const moment = require('moment')
 const Anilist = require('anilist-node')
+const { findAnime } = require('../services/MALService')
+// const fetchDiscussions = require('../fetch/fetchDiscussions')
+// const fetchAssets = require('../fetch/fetchAssets')
+// const cpoll = require('../tools/calculatePoll')
+// const logger = require('../logger')
+// const {
+//   Show, Asset, MALSnapshot, RedditPollResult, Week, Season, EpisodeResultLink, EpisodeDiscussion, EpisodeDiscussionResult, RedditUserScore, Op,
+// } = require('../models')
+
 const aniClient = new Anilist()
 
-//create animetrics anilist app 
+// create animetrics anilist app
 
 function getAnilistUrl(text) {
   const url = text.match(/https?:\/\/(www\.)?(\w*anilist\w*)\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]anime\/[0-9]*)/gm)
@@ -28,9 +30,11 @@ function parseAnilistId(post) {
 
 async function fixMyHeroAcademia() {
   try {
-    throw new Error('omg an error happened line 31')
+    // const resp = await aniClient.media.anime(101367)
+    const resp = await findAnime(37576)
+    console.log(resp)
   } catch (err) {
-    logger.error(err)
+    console.log(err)
   }
 }
 fixMyHeroAcademia()
