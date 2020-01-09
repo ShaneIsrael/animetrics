84629417a85286bf9c28c93673732c83bd4590ec
shaneisrael <shanemisrael@gmail.com>
2020-01-08 20:47:56 -0700

    use real-ip

--- server/src/app.js
+++ server/src/app.js
@@ -45,7 +45,7 @@ require('./cron')
 
 // Error Handler Middleware
 app.use((err, req, res, next) => {
-  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip} - ${err.stack}`)
+  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.headers['x-real-ip']} - ${err.stack}`)
   // This will get passed back to the user as a generic server error
   // message for caught errors.
   res.status(500).send('Unexpected server error occurred.')
