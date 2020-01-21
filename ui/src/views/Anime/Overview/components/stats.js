b1baaaeaf1b9a5e3b6ead9a4650107f35e1d9cca
Shane Israel <sisrael@sofi.org>
2020-01-21 07:58:54 -0700

    updated ratings graph to have the reddit poll score stand out more since its the most useful poll

--- ui/src/views/Anime/Overview/components/stats.js
+++ ui/src/views/Anime/Overview/components/stats.js
@@ -139,15 +139,15 @@ export default function Stats(props) {
             >
               <defs>
                 <linearGradient id="colorRal" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor={theme.palette.secondary.main} stopOpacity={0.4}/>
+                  <stop offset="5%" stopColor={theme.palette.secondary.main} stopOpacity={0}/>
                   <stop offset="95%" stopColor={theme.palette.secondary.main} stopOpacity={0}/>
                 </linearGradient>
                 <linearGradient id="colorMal" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor={amber[400]} stopOpacity={0.4}/>
+                  <stop offset="5%" stopColor={amber[400]} stopOpacity={0}/>
                   <stop offset="95%" stopColor={amber[400]} stopOpacity={0}/>
                 </linearGradient>
                 <linearGradient id="colorRPoll" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor={theme.palette.primary.main} stopOpacity={0.4}/>
+                  <stop offset="5%" stopColor={theme.palette.primary.main} stopOpacity={0.8}/>
                   <stop offset="95%" stopColor={theme.palette.primary.main} stopOpacity={0}/>
                 </linearGradient>
               </defs>
@@ -164,7 +164,7 @@ export default function Stats(props) {
                 activeDot={{ r: 4 }}
                 dataKey="RedditPollScore"
                 stroke={theme.palette.primary.main}
-                strokeWidth={2}
+                strokeWidth={3}
                 type="monotone"
                 fill="url(#colorRPoll)"
               />
