e49696c01f68f91d9567a1e2f26f365ca1885caf
shaneisrael <shanemisrael@gmail.com>
2020-01-23 20:14:16 -0700

    get base url from origin incase a user is on one of the other domain names

--- ui/src/services/Api.js
+++ ui/src/services/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const HOSTNAME = window.location.href
-const URL = HOSTNAME.indexOf('localhost') === -1 ? 'https://animetrics.co/a/': 'http://localhost:3001'
+const BASE = window.location.origin
+const URL = HOSTNAME.indexOf('localhost') === -1 ? `${BASE}/a/`: 'http://localhost:3001'
 export default () => {  
   return axios.create({
     baseURL: URL
