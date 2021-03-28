7df62e0eaed4ac4f50c1f784a9a002d396d6331f
Shane Israel <shanemisrael@gmail.com>
2021-03-28 08:11:48 -0600

    cdn links set to .co instead of .com

--- server/src/services/ResultsService.js
+++ server/src/services/ResultsService.js
@@ -281,9 +281,9 @@ service.getResultsByOrderAndWeek = async (order, wk) => {
       discussion_href: discussion.post_url,
       result_created_dt: epResult.createdAt,
       discussion_created_dt: discussion.post_created_dt,
-      poster_art: `cdn.animetrics.com/${assets[0].s3_poster_compressed ? assets[0].s3_poster_compressed : assets[0].s3_poster}`,
-      banner_art: `cdn.animetrics.com/${assets[0].s3_banner}`,
-      avatar_art: `cdn.animetrics.com/${assets[0].s3_avatar}`,
+      poster_art: `cdn.animetrics.co/${assets[0].s3_poster_compressed ? assets[0].s3_poster_compressed : assets[0].s3_poster}`,
+      banner_art: `cdn.animetrics.co/${assets[0].s3_banner}`,
+      avatar_art: `cdn.animetrics.co/${assets[0].s3_avatar}`,
     })
   }
   if (order === 'karma') {
